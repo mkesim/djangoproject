4fb03e0e7a21b03d9e861f696458434091087326
Mehmet Kesimalioğlu <mkesim96@gmail.com>
2024-06-30 23:28:34 +0000

    main

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,33 @@
+# core/views.py
 from django.shortcuts import render
 
 def index(request):
-    context = {
-        "title": "Django example",
-    }
-    return render(request, "index.html", context)
+    return render(request, 'index.html')
+
+def calculate(request):
+    result = None
+    if request.method == 'POST':
+        num1 = request.POST.get('num1')
+        num2 = request.POST.get('num2')
+        operation = request.POST.get('operation')
+        
+        if num1 and num2:
+            num1 = float(num1)
+            num2 = float(num2)
+            if operation == 'add':
+                result = num1 + num2
+            elif operation == 'subtract':
+                result = num1 - num2
+            elif operation == 'multiply':
+                result = num1 * num2
+            elif operation == 'divide':
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    result = 'Error: Division by zero'
+            else:
+                result = 'Invalid Operation'
+        else:
+            result = 'Please provide both numbers'
+
+    return render(request, 'index.html', {'result': result})
